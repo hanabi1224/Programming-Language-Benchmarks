1ebe3ace72a5d81be04b23444abb7d26a872d81a
hanabi1224 <harlowmoo@gmail.com>
2021-02-01 00:38:38 +0800

    feat: site styles

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -33,6 +33,8 @@ const config: NuxtConfig = {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    // https://google-analytics.nuxtjs.org/
+    '@nuxtjs/google-analytics',
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/stylelint
@@ -50,6 +52,11 @@ const config: NuxtConfig = {
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
   ],
+
+  googleAnalytics: {
+    id: process.env.GOOGLE_ANALYTICS_ID, // Use as fallback if no runtime config is provided
+    checkDuplicatedScript: true,
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
