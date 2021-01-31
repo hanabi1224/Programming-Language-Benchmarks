661cd88d5c913825ab0ab2cf599adcc05bb2669b
hanabi1224 <harlowmoo@gmail.com>
2021-01-31 22:42:44 +0800

    feat: measure child process

---
+++ bench/algorithm/fannkuch-redux/5.rs
@@ -0,0 +1,176 @@
+// The Computer Language Benchmarks Game
+// https://salsa.debian.org/benchmarksgame-team/benchmarksgame/
+//
+// Contributed by Cliff L. Biffle, translated from Jeremy Zerfas's C program.
+//
+// The C program was based on the Ada program by Jonathan Parker and Georg
+// Bauhaus which in turn was based on code by Dave Fladebo, Eckehard Berns,
+// Heiner Marxen, Hongwei Xi, and The Anh Tran and also the Java program by Oleg
+// Mazurov.
+
+extern crate rayon;
+
+use rayon::prelude::*;
+use std::mem::replace;
+
+// This value controls how many blocks the workload is broken up into (as long
+// as the value is less than or equal to the factorial of the argument to this
+// program) in order to allow the blocks to be processed in parallel if
+// possible. PREFERRED_NUMBER_OF_BLOCKS_TO_USE should be some number which
+// divides evenly into all factorials larger than it. It should also be around
+// 2-8 times the amount of threads you want to use in order to create enough
+// blocks to more evenly distribute the workload amongst the threads.
+const PREFERRED_NUMBER_OF_BLOCKS_TO_USE: usize = 12;
+
+// One greater than the maximum `n` value. Used to size stack arrays.
+const MAX_N: usize = 16;
+
+fn main() {
+    let n = std::env::args().nth(1).unwrap().parse().unwrap();
+
+    // This assert eliminates several bounds checks.
+    assert!(n < MAX_N);
+
+    // Create and initialize factorial_lookup_table.
+    let factorial_lookup_table = {
+        let mut table: [usize; MAX_N] = [0; MAX_N];
+        table[0] = 1;
+        for i in 1..MAX_N {
+            table[i] = i * table[i - 1];
+        }
+        table
+    };
+
+    // Determine the block_size to use. If n! is less than
+    // PREFERRED_NUMBER_OF_BLOCKS_TO_USE then just use a single block to prevent
+    // block_size from being set to 0. This also causes smaller values of n to
+    // be computed serially which is faster and uses less resources for small
+    // values of n.
+    let block_size =
+        1.max(factorial_lookup_table[n] / PREFERRED_NUMBER_OF_BLOCKS_TO_USE);
+    let block_count = factorial_lookup_table[n] / block_size;
+
+    // Iterate over each block.
+    let (checksum, max_flip_count) = (0..block_count)
+        .into_par_iter()
+        .map(|bn| {
+            let initial_permutation_index = bn * block_size;
+
+            let mut count: [usize; MAX_N] = [0; MAX_N];
+            let mut current_permutation: [u8; MAX_N] =
+                [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+            // Initialize count and current_permutation.
+            {
+                let mut temp_permutation: [u8; MAX_N] = [0; MAX_N];
+                let mut permutation_index = initial_permutation_index;
+                for i in (1..n).rev() {
+                    let f = factorial_lookup_table[i];
+                    let d = permutation_index / f;
+
+                    count[i] = d;
+
+                    // Rotate the permutation left by d places. This is faster
+                    // than using slice::rotate_left.
+                    temp_permutation[0..=i - d]
+                        .copy_from_slice(&current_permutation[d..=i]);
+                    temp_permutation[i - d + 1..=i]
+                        .copy_from_slice(&current_permutation[..d]);
+                    current_permutation = temp_permutation;
+
+                    permutation_index = permutation_index % f;
+                }
+            }
+
+            let mut max_flip_count = 0;
+            let mut checksum = 0;
+
+            // Iterate over each permutation in the block.
+            let last_permutation_index = initial_permutation_index + block_size;
+            for permutation_index in
+                initial_permutation_index..last_permutation_index
+            {
+                // If the first value in the current_permutation is not 1 (0)
+                // then we will need to do at least one flip for the
+                // current_permutation.
+                if current_permutation[0] > 0 {
+                    // Make a copy of current_permutation[] to work on.
+                    let mut temp_permutation = current_permutation;
+
+                    let mut flip_count: usize = 1;
+
+                    // Flip temp_permutation until the element at the
+                    // first_value index is 1 (0).
+                    let mut first_value = current_permutation[0] as usize & 0xF;
+                    while temp_permutation[first_value] > 0 {
+                        // Record the new_first_value and restore the old
+                        // first_value at its new flipped position.
+                        let new_first_value = replace(
+                            &mut temp_permutation[first_value],
+                            first_value as u8,
+                        );
+
+                        // If first_value is greater than 3 (2) then we are
+                        // flipping a series of four or more values so we will
+                        // also need to flip additional elements in the middle
+                        // of the temp_permutation.
+                        if first_value > 2 {
+                            for (low_index, high_index) in
+                                (1..first_value).zip((1..first_value).rev())
+                            {
+                                temp_permutation.swap(high_index, low_index);
+
+                                if low_index + 3 > high_index {
+                                    break;
+                                }
+                            }
+                        }
+
+                        // Update first_value to new_first_value that we
+                        // recorded earlier.
+                        first_value = new_first_value as usize & 0xF;
+                        flip_count += 1;
+                    }
+
+                    // Update the checksum.
+                    if permutation_index % 2 == 0 {
+                        checksum += flip_count;
+                    } else {
+                        checksum -= flip_count;
+                    }
+
+                    // Update max_flip_count if necessary.
+                    max_flip_count = max_flip_count.max(flip_count);
+                }
+
+                // Generate the next permutation.
+                current_permutation.swap(0, 1);
+                let mut first_value = current_permutation[0];
+                for i in 1..MAX_N - 2 {
+                    count[i] += 1;
+                    if count[i] <= i {
+                        break;
+                    }
+                    count[i] = 0;
+
+                    let new_first_value = current_permutation[1];
+
+                    for j in 0..i + 1 {
+                        current_permutation[j] = current_permutation[j + 1];
+                    }
+
+                    current_permutation[i + 1] = first_value;
+                    first_value = new_first_value;
+                }
+            }
+            (checksum, max_flip_count)
+        })
+        .reduce(
+            || (0, 0),
+            |(cs1, mf1), (cs2, mf2)| (cs1 + cs2, mf1.max(mf2)),
+        );
+
+    // Output the results to stdout.
+    println!("{}", checksum);
+    println!("Pfannkuchen({}) = {}", n, max_flip_count);
+}
---
+++ bench/algorithm/fasta/5.rs
@@ -0,0 +1,268 @@
+// The Computer Language Benchmarks Game
+// https://salsa.debian.org/benchmarksgame-team/benchmarksgame/
+//
+// contributed by the Rust Project Developers
+// contributed by TeXitoi
+// multi-threaded version contributed by Alisdair Owens
+// improvements made to Rust #2 by pmarcelll
+//     * deleted floor() from normalize() since p is always nonnegative
+//     * switched from AoS to SoA (idea borrowed from the C++ solution)
+//     * also preallocate the whole buffer for the data arrays
+//     * cleaned up code a bit (reordering, renaming, formatting, etc.)
+
+extern crate num_cpus;
+
+use std::cmp::min;
+use std::io;
+use std::io::{BufWriter, ErrorKind, Write};
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+const LINE_LENGTH: usize = 60;
+const IM: u32 = 139968;
+const LINES: usize = 1024;
+const BLKLEN: usize = LINE_LENGTH * LINES;
+
+struct MyRandom {
+    last: u32,
+    count: usize,
+    thread_count: u16,
+    next_thread_num: u16,
+}
+
+impl MyRandom {
+    fn new(count: usize, thread_count: u16) -> MyRandom {
+        MyRandom {
+            last: 42,
+            count: count,
+            thread_count: thread_count,
+            next_thread_num: 0,
+        }
+    }
+
+    fn normalize(p: f32) -> u32 {
+        (p * IM as f32) as u32
+    }
+
+    fn reset(&mut self, count: usize) {
+        self.next_thread_num = 0;
+        self.count = count;
+    }
+
+    fn gen(&mut self, buf: &mut [u32], cur_thread: u16) -> Result<usize, ()> {
+        if self.next_thread_num != cur_thread {
+            return Err(());
+        }
+
+        self.next_thread_num += 1;
+        if self.next_thread_num == self.thread_count {
+            self.next_thread_num = 0;
+        }
+
+        let to_gen = min(buf.len(), self.count);
+        for i in 0..to_gen {
+            self.last = (self.last * 3877 + 29573) % IM;
+            buf[i] = self.last;
+        }
+        self.count -= to_gen;
+        Ok(to_gen)
+    }
+}
+
+struct MyStdOut {
+    thread_count: u16,
+    next_thread_num: u16,
+    stdout: io::Stdout,
+}
+
+impl MyStdOut {
+    fn new(thread_count: u16) -> MyStdOut {
+        MyStdOut {
+            thread_count: thread_count,
+            next_thread_num: 0,
+            stdout: io::stdout(),
+        }
+    }
+
+    fn write(&mut self, data: &[u8], cur_thread: u16) -> io::Result<()> {
+        if self.next_thread_num != cur_thread {
+            return Err(io::Error::new(ErrorKind::Other, ""));
+        }
+
+        self.next_thread_num += 1;
+        if self.next_thread_num == self.thread_count {
+            self.next_thread_num = 0;
+        }
+
+        self.stdout.write_all(data)
+    }
+}
+
+fn make_random(data: &[(char, f32)]) -> (Box<[u32]>, Box<[u8]>) {
+    let mut acc = 0.;
+    let mut buf_p = Vec::with_capacity(data.len());
+    let mut buf_ch = Vec::with_capacity(data.len());
+
+    for &(ch, p) in data {
+        acc += p;
+        buf_p.push(MyRandom::normalize(acc));
+        buf_ch.push(ch as u8);
+    }
+
+    (buf_p.into(), buf_ch.into())
+}
+
+fn make_fasta_single<I: Iterator<Item = u8>>(
+    header: &str,
+    mut it: I,
+    mut n: usize,
+) -> io::Result<()> {
+    let mut stdout = BufWriter::new(io::stdout());
+    stdout.write_all(header.as_bytes())?;
+    let mut line = [0u8; LINE_LENGTH + 1];
+    while n > 0 {
+        let nb = min(LINE_LENGTH, n);
+        for i in 0..nb {
+            line[i] = it.next().unwrap();
+        }
+        n -= nb;
+        line[nb] = '\n' as u8;
+        stdout.write_all(&line[..(nb + 1)])?;
+    }
+    Ok(())
+}
+
+fn do_fasta(
+    thread_num: u16,
+    rng: Arc<Mutex<MyRandom>>,
+    wr: Arc<Mutex<MyStdOut>>,
+    data: (Box<[u32]>, Box<[u8]>),
+) {
+    let mut rng_buf = [0u32; BLKLEN];
+    let mut out_buf = [0u8; BLKLEN + LINES];
+    let mut count;
+    loop {
+        loop {
+            if let Ok(x) = rng.lock().unwrap().gen(&mut rng_buf, thread_num) {
+                count = x;
+                break;
+            }
+        }
+
+        if count == 0 {
+            break;
+        }
+        let mut line_count = 0;
+        for i in 0..count {
+            if i % LINE_LENGTH == 0 && i > 0 {
+                out_buf[i + line_count] = b'\n';
+                line_count += 1;
+            }
+            let rn = rng_buf[i];
+            for j in data.0.iter().zip(data.1.iter()) {
+                if *j.0 >= rn {
+                    out_buf[i + line_count] = *j.1;
+                    break;
+                }
+            }
+        }
+        out_buf[count + line_count] = b'\n';
+
+        while let Err(_) = wr.lock()
+            .unwrap()
+            .write(&out_buf[..(count + line_count + 1)], thread_num)
+        {}
+    }
+}
+
+fn make_fasta(
+    header: &str,
+    rng: Arc<Mutex<MyRandom>>,
+    data: (Box<[u32]>, Box<[u8]>),
+    num_threads: u16,
+) -> io::Result<()> {
+    let stdout = Arc::new(Mutex::new(MyStdOut::new(num_threads)));
+    io::stdout().write_all(header.as_bytes())?;
+    let mut threads = Vec::with_capacity(num_threads as usize);
+    for thread in 0..num_threads {
+        let data = data.clone();
+        let rng = rng.clone();
+        let stdout = stdout.clone();
+        threads.push(thread::spawn(move || {
+            do_fasta(thread, rng, stdout, data);
+        }));
+    }
+    for thread_guard in threads {
+        thread_guard.join().unwrap();
+    }
+    Ok(())
+}
+
+fn main() {
+    let n = std::env::args_os()
+        .nth(1)
+        .and_then(|s| s.into_string().ok())
+        .and_then(|n| n.parse().ok())
+        .unwrap_or(1000);
+
+    let num_threads: u16 = num_cpus::get() as u16;
+
+    let rng = Arc::new(Mutex::new(MyRandom::new(n * 3, num_threads)));
+    let alu: &[u8] = b"GGCCGGGCGCGGTGGCTCACGCCTGTAATCCCAGCACTTT\
+                       GGGAGGCCGAGGCGGGCGGATCACCTGAGGTCAGGAGTTC\
+                       GAGACCAGCCTGGCCAACATGGTGAAACCCCGTCTCTACT\
+                       AAAAATACAAAAATTAGCCGGGCGTGGTGGCGCGCGCCTG\
+                       TAATCCCAGCTACTCGGGAGGCTGAGGCAGGAGAATCGCT\
+                       TGAACCCGGGAGGCGGAGGTTGCAGTGAGCCGAGATCGCG\
+                       CCACTGCACTCCAGCCTGGGCGACAGAGCGAGACTCCGTCT\
+                       CAAAAA";
+
+    let iub = &[
+        ('a', 0.27),
+        ('c', 0.12),
+        ('g', 0.12),
+        ('t', 0.27),
+        ('B', 0.02),
+        ('D', 0.02),
+        ('H', 0.02),
+        ('K', 0.02),
+        ('M', 0.02),
+        ('N', 0.02),
+        ('R', 0.02),
+        ('S', 0.02),
+        ('V', 0.02),
+        ('W', 0.02),
+        ('Y', 0.02),
+    ];
+
+    let homosapiens = &[
+        ('a', 0.3029549426680),
+        ('c', 0.1979883004921),
+        ('g', 0.1975473066391),
+        ('t', 0.3015094502008),
+    ];
+
+    make_fasta_single(
+        ">ONE Homo sapiens alu\n",
+        alu.iter().cycle().map(|c| *c),
+        n * 2,
+    ).unwrap();
+
+    make_fasta(
+        ">TWO IUB ambiguity codes\n",
+        rng.clone(),
+        make_random(iub),
+        num_threads,
+    ).unwrap();
+
+    rng.lock().unwrap().reset(n * 5);
+
+    make_fasta(
+        ">THREE Homo sapiens frequency\n",
+        rng,
+        make_random(homosapiens),
+        num_threads,
+    ).unwrap();
+
+    io::stdout().flush().unwrap();
+}
