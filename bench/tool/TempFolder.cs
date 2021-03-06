2e95f228fe996a766ef444a378f7693a4764ae2e
hanabi1224 <harlowmoo@gmail.com>
2021-03-07 00:04:21 +0800

    no warn

--- bench/tool/TempFolder.cs
+++ bench/tool/TempFolder.cs
@@ -16,11 +16,10 @@ namespace BenchTool
 
         public string RootDirName { get; set; }
 
-        public TempFolder()
+        public TempFolder(string identifier = "")
         {
-            RootDirName = Path.GetFileNameWithoutExtension(Path.GetRandomFileName());
-            //FullPath = Path.Combine(Environment.CurrentDirectory, ".tmp", RootDirName);
-            FullPath = Path.Combine(Path.GetTempPath(), RootDirName);
+            RootDirName = $"{identifier}_{Path.GetFileNameWithoutExtension(Path.GetRandomFileName())}";
+            FullPath = Path.Combine(Path.GetTempPath(),  RootDirName);
         }
 
         public void CreateIfNotExist()
