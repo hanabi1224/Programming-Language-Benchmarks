e5f5e3d514b8d95e7e181e5ec54f28ba52529bd6
hanabi1224 <harlowmoo@gmail.com>
2021-03-21 04:46:29 +0800

    updt

---
+++ bench/include/rust/wrapper.js
@@ -0,0 +1,17 @@
+'use strict';
+const fs = require('fs');
+const { WASI } = require('wasi');
+
+const wasi = new WASI({
+    args: process.argv,
+    env: process.env,
+});
+
+const importObject = { wasi_snapshot_preview1: wasi.wasiImport };
+
+(async () => {
+    const wasm = await WebAssembly.compile(fs.readFileSync('./_app.wasm'));
+    const instance = await WebAssembly.instantiate(wasm, importObject);
+
+    wasi.start(instance);
+})();
