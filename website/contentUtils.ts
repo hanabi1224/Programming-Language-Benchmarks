de420a1e79d8440e3057ed9fad7992f1ebbcd9de
hanabi1224 <harlowmoo@gmail.com>
2021-03-31 11:09:47 +0800

    semver (#78)

--- website/contentUtils.ts
+++ website/contentUtils.ts
@@ -1,14 +1,15 @@
-import { contentFunc, IContentDocument } from '@nuxt/content/types/content';
-import _ from 'lodash';
+import { contentFunc, IContentDocument } from '@nuxt/content/types/content'
+import _ from 'lodash'
+import findVersions from 'find-versions'
 
 const lang2Display: { [key: string]: string } = {
   'csharp': 'C#',
-};
+}
 
 export async function getLangBenchResults($content: contentFunc) {
-  const pages = await $content('/', { deep: true }).fetch() as IContentDocument[];
-  var benchResults = pages as unknown as BenchResult[];
-  return mergeLangBenchResults(benchResults);
+  const pages = await $content('/', { deep: true }).fetch() as IContentDocument[]
+  var benchResults = pages as unknown as BenchResult[]
+  return mergeLangBenchResults(benchResults)
 }
 
 export function mergeLangBenchResults(benchResults: BenchResult[]) {
@@ -34,6 +35,16 @@ export function getFullCompilerVersion(i: BenchResult) {
 
 export function getRealShortCompilerVersion(i: BenchResult) {
   const full = getFullCompilerVersion(i)
-  const match = /\d+\.\d+(\.\d+)?/.exec(full)
-  return match ? match[0] : 'unknown'
+  const versions = findVersions(full, { loose: true })
+  if (versions && versions.length > 0) {
+    return versions[0]
+  }
+
+  // "Backfill for openjdk version 16"
+  const match = /\d+(\.\d+){0,3}([_+]\d+)?/.exec(full)
+  if (match) {
+    return match[0]
+  }
+
+  return 'unknown'
 }
