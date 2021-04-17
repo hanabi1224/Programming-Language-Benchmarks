a5b92c803f8a649423d46f419be141b4fe920f33
hanabi1224 <harlowmoo@gmail.com>
2021-04-17 10:01:18 +0800

    prime sieve

---
+++ bench/algorithm/prime-sieve/1.cs
@@ -0,0 +1,63 @@
+// Port from go concurrent prime sieve with goroutines
+
+using System;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+static class ConcurrentPrimeSieve
+{
+    const int ChannelSize = 2;
+    public static async Task Main(string[] args)
+    {
+        int n;
+        if (args.Length == 0 || !int.TryParse(args[0], out n))
+        {
+            n = 27;
+        }
+
+        using var cts = new CancellationTokenSource();
+        var ch = Channel.CreateBounded<int>(ChannelSize);
+        _ = GenerateAsync(ch.Writer);
+        for (var i = 0; ; i++)
+        {
+            var prime = await ch.Reader.ReadAsync().ConfigureAwait(false);
+            Console.WriteLine(prime);
+            if (i >= n)
+            {
+                break;
+            }
+            var chNext = Channel.CreateBounded<int>(ChannelSize);
+            _ = FilterAsync(ch.Reader, chNext.Writer, prime, cts.Token);
+            ch = chNext;
+        }
+
+        cts.Cancel();
+    }
+
+    static async Task GenerateAsync(ChannelWriter<int> writer)
+    {
+        await Task.Yield();
+        for (var i = 2; ; i++)
+        {
+            await writer.WriteAsync(i).ConfigureAwait(false);
+        }
+    }
+
+    static async Task FilterAsync(
+        ChannelReader<int> reader,
+        ChannelWriter<int> writer,
+        int prime,
+        CancellationToken ct)
+    {
+        await Task.Yield();
+        while (!ct.IsCancellationRequested)
+        {
+            var n = await reader.ReadAsync(ct).ConfigureAwait(false);
+            if (n % prime != 0)
+            {
+                await writer.WriteAsync(n, ct).ConfigureAwait(false);
+            }
+        }
+    }
+}
