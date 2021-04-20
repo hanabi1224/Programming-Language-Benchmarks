04e31ce73a892924d8754b6fcdaaa302b68257c0
hanabi1224 <harlowmoo@gmail.com>
2021-04-20 21:44:54 +0800

    nim pidigits edigits (#108)

---
+++ bench/algorithm/prime-sieve/1.js
@@ -0,0 +1,32 @@
+// Not true coroutine w/o fiber tho
+
+async function* generate() {
+    for(var i = 2; ;i++){
+        yield i;
+    }
+}
+
+async function* filter(ch, prime) {
+    while(true) {
+        var i = (await ch.next()).value;
+        if (i % prime != 0) {
+            yield i;
+        }
+    }
+}
+
+async function findPrimes(n) {
+    var ch = generate();
+    for (var i = 0; i < n; i++) {
+        const prime = (await ch.next()).value;
+        console.log(prime);
+        ch = filter(ch, prime);
+    }
+}
+
+function main() {
+    const n = +process.argv[2] || 100;
+    findPrimes(n);    
+}
+
+main();
