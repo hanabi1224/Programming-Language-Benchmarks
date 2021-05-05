29147f3a2d0d064a1760b8a810b9eff9001783fa
hanabi1224 <harlowmoo@gmail.com>
2021-05-05 18:29:36 +0800

    bb

--- bench/include/kotlin-jvm/build.gradle.kts
+++ bench/include/kotlin-jvm/build.gradle.kts
@@ -4,7 +4,7 @@ object Constants {
 
 plugins {
     kotlin("jvm").version("1.5.0")
-    id("com.github.johnrengelman.shadow").version("7.0.0")
+    id("com.github.johnrengelman.shadow").version("6.1.0")
     java
     application
 }
