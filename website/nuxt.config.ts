ada4fbab42bed9d3dc72613a8859ce13797f7f1a
hanabi1224 <harlowmoo@gmail.com>
2021-07-05 21:25:00 +0800

    Update nuxt.config.ts

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -147,11 +147,6 @@ if (process.env.APP_HOST_NAME) {
   config.buildModules?.push('@nuxtjs/sitemap')
   config.sitemap = {
     hostname: process.env.APP_HOST_NAME,
-    defaults: {
-      changefreq: 'weekly',
-      priority: 1,
-      lastmod: new Date(),
-    },
   }
 }
 
