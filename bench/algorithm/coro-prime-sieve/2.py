067c6b46bb7864f1b8f2ed7f3941f6967b8026a0
hanabi1224 <harlowmoo@gmail.com>
2021-07-09 10:55:29 +0800

    More coro-prime-sieve impls (#133)

--- bench/algorithm/coro-prime-sieve/2.py
+++ bench/algorithm/coro-prime-sieve/2.py
@@ -1,28 +1,28 @@
 import sys
-import asyncio
 
 
-async def main():
+def main():
     n = 100 if len(sys.argv) < 2 else int(sys.argv[1])
     ch = generate()
     for i in range(0, n):
-        prime = await ch.__anext__()
+        prime = ch.__next__()
         print(prime)
         ch = filter(ch, prime)
 
 
-async def generate():
+def generate():
     i = 2
     while True:
         yield i
         i += 1
 
 
-async def filter(ch, prime):
-    async for i in ch:
+def filter(ch, prime):
+    for i in ch:
         if i % prime != 0:
             yield i
 
+
 if __name__ == '__main__':
     sys.setrecursionlimit(5000)
-    asyncio.run(main())
+    main()
