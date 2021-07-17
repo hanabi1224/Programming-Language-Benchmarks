78b013bc5bed65c2f27084931b78f8aa471149a0
hanabi1224 <harlowmoo@gmail.com>
2021-07-17 14:35:56 +0800

    crystal impl of http-server

--- bench/include/kotlin-native/build.gradle.kts
+++ bench/include/kotlin-native/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 object Constants {
     const val kotlinVersion = "1.5.21"
 }
@@ -6,6 +8,7 @@ plugins {
     val kotlinVersion = "1.5.21"
     kotlin("multiplatform").version(kotlinVersion)
     kotlin("plugin.serialization").version(kotlinVersion)
+    id("com.github.ben-manes.versions").version("0.39.0")
 }
 
 repositories {
@@ -38,4 +41,20 @@ tasks.register("kotlinVersion") {
     // $HOME/.konan/kotlin-native-prebuilt-linux-${Constants.kotlinVersion}/bin/kotlinc-native -version
     println("Kotlin/Native ${Constants.kotlinVersion}")
   }
+}
+
+tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
+  rejectVersionIf { isNonStable(candidate.version) && !isNonStable(currentVersion) }
+  // optional parameters
+  checkForGradleUpdate = true
+  outputFormatter = "json"
+  outputDir = "build/dependencyUpdates"
+  reportfileName = "report"
+}
+
+fun isNonStable(version: String): Boolean {
+  val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+  val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+  val isStable = stableKeyword || regex.matches(version)
+  return isStable.not()
 }
