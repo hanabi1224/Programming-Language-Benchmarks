8b04fdc519f59cb59e4a52df4757fb7020028941
hanabi1224 <harlowmoo@gmail.com>
2021-09-07 02:17:58 +0800

    revert GH action change

--- bench/include/kotlin-jvm/build.gradle.kts
+++ bench/include/kotlin-jvm/build.gradle.kts
@@ -25,7 +25,7 @@ application {
 
 dependencies {
     // implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
     val ktor_version = "1.6.3"
     implementation("io.ktor:ktor-server-core:$ktor_version")
