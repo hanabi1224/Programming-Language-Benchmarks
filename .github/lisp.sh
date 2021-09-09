02116611b3fcf00a2af1d63df2c27a70a6e94906
Bela Pecsek <bela.pecsek@gmail.com>
2021-09-09 16:20:38 +0200

    list-all-packages added

--- .github/lisp.sh
+++ .github/lisp.sh
@@ -16,3 +16,4 @@ curl -O https://beta.quicklisp.org/quicklisp.lisp
 sbcl --noinform --load quicklisp.lisp --eval "(quicklisp-quickstart:install)" --eval "(ql-util:without-prompting (ql:add-to-init-file))" --eval "(exit)"
 git clone https://github.com/marcoheisig/sb-simd.git $HOME/quicklisp/local-projects/sb-simd
 sbcl --noinform --eval "(ql:quickload :sb-simd)" --eval "(exit)"
+sbcl --eval "(list-all-packages)"
