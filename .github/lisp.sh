9d59b5613c252c5ebb3d35d2bcc3c25127d77f52
Bela Pecsek <bela.pecsek@gmail.com>
2021-09-09 15:56:36 +0200

    Spectralnorm 2.cl with sb-simd support

--- .github/lisp.sh
+++ .github/lisp.sh
@@ -6,7 +6,7 @@ echo 'ros run -- $@' >>./sbcl
 cat ./sbcl
 sudo chmod +x ./sbcl
 curl -L https://raw.githubusercontent.com/roswell/roswell/master/scripts/install-for-ci.sh | sh
-sudo ros install sbcl-bin
+sudo ros install sbcl-bin --without-immobile-code
 sudo ros use sbcl-bin
 ros version
 ./sbcl --version
@@ -14,5 +14,5 @@ sudo mv ./sbcl /usr/bin
 sbcl --version
 curl -O https://beta.quicklisp.org/quicklisp.lisp
 sbcl --noinform --load quicklisp.lisp --eval "(quicklisp-quickstart:install)" --eval "(ql-util:without-prompting (ql:add-to-init-file))" --eval "(exit)"
-#git clone https://github.com/marcoheisig/sb-simd.git $HOME/quicklisp/local-projects/sb-simd
-#sbcl --noinform --eval "(ql:quickload :sb-simd)" --eval "(exit)"
+git clone https://github.com/marcoheisig/sb-simd.git $HOME/quicklisp/local-projects/sb-simd
+sbcl --noinform --eval "(ql:quickload :sb-simd)" --eval "(exit)"
