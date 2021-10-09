b01de33f5272ec9a04a41a89d15f0d8e4426b3e4
Bela Pecsek <50743731+bpecsek@users.noreply.github.com>
2021-10-09 11:04:48 +0200

    Update lisp.sh

--- .github/lisp.sh
+++ .github/lisp.sh
@@ -10,6 +10,6 @@ cd $HOME
 sbcl --version
 curl -O https://beta.quicklisp.org/quicklisp.lisp
 sbcl --noinform --load quicklisp.lisp --eval "(quicklisp-quickstart:install)" --eval "(ql-util:without-prompting (ql:add-to-init-file))" --eval "(exit)"
-git clone https://github.com/marcoheisig/sb-simd.git $HOME/quicklisp/local-projects/sb-simd
+git clone https://github.com/bpecsek/sb-simd.git $HOME/quicklisp/local-projects/sb-simd
 sbcl --noinform --eval "(ql:quickload :sb-simd)" --eval "(exit)"
 sbcl --version
