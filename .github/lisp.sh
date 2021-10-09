de8539e4675cf65d04a7d1a0dd454b787facb278
Bela Pecsek <50743731+bpecsek@users.noreply.github.com>
2021-10-09 11:48:44 +0200

    Update lisp.sh

--- .github/lisp.sh
+++ .github/lisp.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/sh
 sudo apt install sbcl
 sbcl --version
-git clone https://github.com/bpecsek/sbcl.git $HOME/sbcl
+git clone https://github.com/sbcl/sbcl.git $HOME/sbcl
+#git clone https://github.com/bpecsek/sbcl.git $HOME/sbcl
 cd $HOME/sbcl
 #git checkout sbcl-2.1.9
 sh ./make.sh --prefix=/usr --fancy
@@ -10,6 +11,6 @@ cd $HOME
 sbcl --version
 curl -O https://beta.quicklisp.org/quicklisp.lisp
 sbcl --noinform --load quicklisp.lisp --eval "(quicklisp-quickstart:install)" --eval "(ql-util:without-prompting (ql:add-to-init-file))" --eval "(exit)"
-git clone https://github.com/bpecsek/sb-simd.git $HOME/quicklisp/local-projects/sb-simd
+git clone  https://github.com/marcoheisig/sb-simd.git $HOME/quicklisp/local-projects/sb-simd
 sbcl --noinform --eval "(ql:quickload :sb-simd)" --eval "(exit)"
 sbcl --version
