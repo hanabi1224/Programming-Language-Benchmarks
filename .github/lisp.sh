a51755b50212cd1cee88f6dba68a4de00c382792
Bela Pecsek <50743731+bpecsek@users.noreply.github.com>
2021-10-09 13:50:32 +0200

    Updated lisp.sh to build properly for latest sb-simd with development branch of sbcl

--- .github/lisp.sh
+++ .github/lisp.sh
@@ -3,7 +3,7 @@ sudo apt install sbcl
 sbcl --version
 git clone https://github.com/sbcl/sbcl $HOME/sbcl
 cd $HOME/sbcl
-git checkout sbcl-2.1.9
+git checkout 77bd1bf6f8c81f9e53410c54ae659995e0c25f08
 sh ./make.sh --prefix=/usr --fancy
 sudo sh ./install.sh
 cd $HOME
@@ -11,5 +11,8 @@ sbcl --version
 curl -O https://beta.quicklisp.org/quicklisp.lisp
 sbcl --noinform --load quicklisp.lisp --eval "(quicklisp-quickstart:install)" --eval "(ql-util:without-prompting (ql:add-to-init-file))" --eval "(exit)"
 git clone https://github.com/marcoheisig/sb-simd.git $HOME/quicklisp/local-projects/sb-simd
+cd $HOME/quicklisp/local-projects/sb-simd
+git checkout 7c432699affd21516940009bab9e05e1695eee8b
+cd $HOME
 sbcl --noinform --eval "(ql:quickload :sb-simd)" --eval "(exit)"
 sbcl --version
