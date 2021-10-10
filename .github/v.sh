56aee38efa03b60ed4d6655cf8935324a90cd65c
hanabi1224 <harlowmoo@gmail.com>
2021-10-10 21:53:46 +0800

    clang-13 (#179)

--- .github/v.sh
+++ .github/v.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/sh
 
-docker run --rm -v /tmp/hostlib/:/working/ -w /working/ thevlang/vlang:alpine cp /lib/ld-musl-x86_64.so.1 .
-sudo cp /tmp/hostlib/ld-musl-x86_64.so.1 /lib/
-sudo apt install -y libgc-dev
+# docker run --rm -v /tmp/hostlib/:/working/ -w /working/ thevlang/vlang:alpine cp /lib/ld-musl-x86_64.so.1 .
+# sudo cp /tmp/hostlib/ld-musl-x86_64.so.1 /lib/
+sudo apt-get install -y libgc-dev
 git clone https://github.com/vlang/v /tmp/vlang
 cd /tmp/vlang
 make && ./v -version
