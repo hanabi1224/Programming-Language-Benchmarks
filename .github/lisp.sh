07ee9dccd5b648316576692d54df6a4f872bce2e
Bela Pecsek <50743731+bpecsek@users.noreply.github.com>
2021-10-14 14:37:16 +0200

    Latest sb-simd and sbcl  (#181)

--- .github/lisp.sh
+++ .github/lisp.sh
@@ -3,7 +3,7 @@ sudo apt install sbcl
 sbcl --version
 git clone https://github.com/sbcl/sbcl $HOME/sbcl
 cd $HOME/sbcl
-git checkout 77bd1bf6f8c81f9e53410c54ae659995e0c25f08
+git checkout 14b60aa20c07481b64756b4a250fe8e6eed307b2
 sh ./make.sh --prefix=/usr --fancy
 sudo sh ./install.sh
 cd $HOME
@@ -12,7 +12,7 @@ curl -O https://beta.quicklisp.org/quicklisp.lisp
 sbcl --noinform --load quicklisp.lisp --eval "(quicklisp-quickstart:install)" --eval "(ql-util:without-prompting (ql:add-to-init-file))" --eval "(exit)"
 git clone https://github.com/marcoheisig/sb-simd.git $HOME/quicklisp/local-projects/sb-simd
 cd $HOME/quicklisp/local-projects/sb-simd
-git checkout f2abc3f5c88322333a6bb648427b498a922ce54e
+git checkout 409111d2025e57888444fffb54d58aba198bdfe6
 cd $HOME
 sbcl --noinform --eval "(ql:quickload :sb-simd)" --eval "(exit)"
 sbcl --version
