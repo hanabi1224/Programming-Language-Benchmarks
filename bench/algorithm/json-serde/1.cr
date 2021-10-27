000a299ba6352bd2f0fd8c12817f5d19527b3a72
hanabi1224 <harlowmoo@gmail.com>
2021-10-28 02:11:31 +0800

    set zig build target cpu to skylake

--- bench/algorithm/json-serde/1.cr
+++ bench/algorithm/json-serde/1.cr
@@ -2,18 +2,11 @@ require "json"
 require "digest"
 
 def print_hash(json)
-  # io = IO::Memory.new
-  # builder = JSON::Builder.new(io)
-  # builder.start_document
-  # json.to_json(builder)
-  # builder.end_document
   hasher = Digest::MD5.new
-  # hasher.update(io.to_s)
-  # puts json.to_json
   hasher.update(json.to_json)
   hash = hasher.final
-  puts hash.map { |b| b.to_s(16) }.join
-end 
+  puts hash.map { |b| b.to_s(16, precision: 2, upcase: false) }.join
+end
 
 file_name = ARGV.size > 0 ? ARGV[0] : "sample"
 n = ARGV.size > 1 ? ARGV[1].to_i : 10
