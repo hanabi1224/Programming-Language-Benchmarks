4e7b949a7cec343bddc7b7f8dad65767e85c94cf
hanabi1224 <harlowmoo@gmail.com>
2021-10-29 19:14:20 +0800

    graalvm java 17

--- .github/graalvm-native-image.sh
+++ .github/graalvm-native-image.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
 sudo gu install native-image
-sudo ln -sf $HOME/graalvm-ce-java11/bin/native-image /usr/bin/native-image
+sudo ln -sf $HOME/graalvm-ce-java17/bin/native-image /usr/bin/native-image
 native-image --version
