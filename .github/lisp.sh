43e25a04214233f83b49881dca3c6b89d1cff474
hanabi1224 <harlowmoo@gmail.com>
2021-11-04 19:45:32 +0800

    sbcl 2.1.10

--- .github/lisp.sh
+++ .github/lisp.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/sh
-sudo apt install sbcl
+sudo apt install -y sbcl
 sbcl --version
 git clone https://github.com/sbcl/sbcl $HOME/sbcl
 cd $HOME/sbcl
-git checkout 14b60aa20c07481b64756b4a250fe8e6eed307b2
+git checkout sbcl-2.1.10
 sh ./make.sh --prefix=/usr --fancy
 sudo sh ./install.sh
 cd $HOME
 sbcl --version
 curl -O https://beta.quicklisp.org/quicklisp.lisp
 sbcl --noinform --load quicklisp.lisp --eval "(quicklisp-quickstart:install)" --eval "(ql-util:without-prompting (ql:add-to-init-file))" --eval "(exit)"
-git clone https://github.com/marcoheisig/sb-simd.git $HOME/quicklisp/local-projects/sb-simd
+git clone https://github.com/marcoheisig/sb-simd $HOME/quicklisp/local-projects/sb-simd
 cd $HOME/quicklisp/local-projects/sb-simd
-git checkout 409111d2025e57888444fffb54d58aba198bdfe6
+git checkout 384c19896acf46a3db80ea3a3785b344fdf1c069
 cd $HOME
 sbcl --noinform --eval "(ql:quickload :sb-simd)" --eval "(exit)"
 sbcl --version
