6f4c67a4a3eff241553ae49a9bbafb96d394c05c
hanabi1224 <harlowmoo@gmail.com>
2021-11-12 05:53:09 +0800

    loom jdk update

--- .github/loom.sh
+++ .github/loom.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
-wget https://download.java.net/java/early_access/loom/2/openjdk-18-loom+2-74_linux-x64_bin.tar.gz
-tar -xvf openjdk-18-loom+2-74_linux-x64_bin.tar.gz
+wget https://download.java.net/java/early_access/loom/4/openjdk-18-loom+4-273_linux-x64_bin.tar.gz
+tar -xvf openjdk-18-loom+4-273_linux-x64_bin.tar.gz
 sudo ln -sf $PWD/jdk-18/bin/javac /usr/bin/loomjavac
 sudo ln -sf $PWD/jdk-18/bin/java /usr/bin/loomjava
 loomjavac -version
