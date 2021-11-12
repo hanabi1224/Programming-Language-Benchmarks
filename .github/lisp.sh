c43ceaeae88b47e31204ead1be3c3e439402da38
Bela Pecsek <bela.pecsek@gmail.com>
2021-11-12 11:24:23 +0100

    Latest sb-simd fails to build under sbcl-2.1.10

--- .github/lisp.sh
+++ .github/lisp.sh
@@ -12,5 +12,7 @@ curl -O https://beta.quicklisp.org/quicklisp.lisp
 sbcl --noinform --load quicklisp.lisp --eval "(quicklisp-quickstart:install)" --eval "(ql-util:without-prompting (ql:add-to-init-file))" --eval "(exit)"
 git clone https://github.com/marcoheisig/sb-simd $HOME/quicklisp/local-projects/sb-simd
 cd $HOME/quicklisp/local-projects/sb-simd
+git checkout a784fe831792764e88368656fa7fd643e33b712b
+cd $HOME
 sbcl --noinform --eval "(ql:quickload :sb-simd)" --eval "(exit)"
 sbcl --version
