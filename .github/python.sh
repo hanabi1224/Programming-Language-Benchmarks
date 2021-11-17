9a6e2df61c810c9ec109006cb5009d3cab0569a1
hanabi1224 <harlowmoo@gmail.com>
2021-11-17 21:47:46 +0800

    pyjion (#193)

--- .github/python.sh
+++ .github/python.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/sh
 
-sudo gu install python
-sudo ln -sf $HOME/graalvm-ce-java17/bin/graalpython /usr/bin/graalpython
-graalpython --version
+python3 --version
+python3 -m pip install -U pyjion
+
+# sudo apt-get install -y dotnet-sdk-6.0
+
+# sudo gu install python
+# sudo ln -sf $HOME/graalvm-ce-java17/bin/graalpython /usr/bin/graalpython
+# graalpython --version
