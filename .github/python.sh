f3dd2d128dccf184d338543f6bc080d3fc355815
hanabi1224 <harlowmoo@gmail.com>
2021-11-17 21:08:16 +0800

    pyjion

--- .github/python.sh
+++ .github/python.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/sh
 
-sudo gu install python
-sudo ln -sf $HOME/graalvm-ce-java17/bin/graalpython /usr/bin/graalpython
-graalpython --version
+python3 --version
+python3 -m pip install -U pyjion
+
+# sudo gu install python
+# sudo ln -sf $HOME/graalvm-ce-java17/bin/graalpython /usr/bin/graalpython
+# graalpython --version
