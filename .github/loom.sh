363e8c75466e34aa8ad1f4a27dc81e581ffc22f5
hanabi1224 <harlowmoo@gmail.com>
2021-12-23 06:23:22 +0800

    latest loom

--- .github/loom.sh
+++ .github/loom.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/sh
 
-wget https://download.java.net/java/early_access/loom/7/openjdk-18-loom+7-288_linux-x64_bin.tar.gz
-tar -xvf openjdk-18-loom+7-288_linux-x64_bin.tar.gz
-sudo ln -sf $PWD/jdk-18/bin/javac /usr/bin/loomjavac
-sudo ln -sf $PWD/jdk-18/bin/java /usr/bin/loomjava
+wget https://download.java.net/java/early_access/loom/1/openjdk-19-loom+1-11_linux-x64_bin.tar.gz
+tar -xvf openjdk-19-loom+1-11_linux-x64_bin.tar.gz
+sudo ln -sf $PWD/jdk-19/bin/javac /usr/bin/loomjavac
+sudo ln -sf $PWD/jdk-19/bin/java /usr/bin/loomjava
 loomjavac -version
 loomjava -version
