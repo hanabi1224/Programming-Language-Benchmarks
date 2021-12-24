408a78e0e8f289cc25c4c25f415fdc79065f6124
hanabi1224 <harlowmoo@gmail.com>
2021-12-24 20:19:29 +0800

    kotlinx-coroutines 1.6

--- bench/include/kotlin-native/build.gradle.kts
+++ bench/include/kotlin-native/build.gradle.kts
@@ -23,8 +23,8 @@ kotlin {
         implementation(libs.bignum)
         implementation(libs.kbignum)
         // implementation("com.ionspin.kotlin:bignum:0.3.1")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
       }
     }
   }
