8e3679eeb20e668dc698a0d11e88e6713127e215
hanabi1224 <harlowmoo@gmail.com>
2022-02-17 11:02:27 +0800

    more cr programs

---
+++ bench/algorithm/nsieve/2.cr
@@ -0,0 +1,20 @@
+require "bit_array"
+
+def nsieve(n)
+  count = 0
+  flags = BitArray.new n
+  (2...n).each do |i|
+    if !flags.unsafe_fetch(i)
+      count += 1
+    end
+    (i*2...n).step(i).each do |j|
+      flags.unsafe_put j, true
+    end
+  end
+  puts "Primes up to %8d %8d" % [n, count]
+end
+
+n = ARGV.size > 0 ? ARGV[0].to_i : 4
+(0...3).each do |i|
+  nsieve(10000 << (n - i))
+end
