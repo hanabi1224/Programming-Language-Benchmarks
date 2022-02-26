5bc3b85f9e696b125f5ccd0f1ccd214f6610690c
hanabi1224 <harlowmoo@gmail.com>
2022-02-26 19:47:40 +0800

    cleanup

--- .github/lua.sh
+++ .github/lua.sh
@@ -6,16 +6,23 @@ curl -R -O http://www.lua.org/ftp/lua-5.4.4.tar.gz
 tar zxf lua-5.4.4.tar.gz
 pushd lua-5.4.4
 make all test
-sudo ln -sf $PWD/src/luac /usr/bin/luac
+# sudo ln -sf $PWD/src/luac /usr/bin/luac
+# sudo ln -sf $PWD/src/lua /usr/bin/lua
+sudo make install
 popd
 popd
-cp /tmp/lua-5.4.4/src/lua ./bench/include/lua/out
+# cp /tmp/lua-5.4.4/src/lua ./bench/include/lua/out
 luac -v
-./bench/include/lua/out/lua -v
+lua -v
+# ./bench/include/lua/out/lua -v
 
-git clone git@github.com:LuaJIT/LuaJIT.git /tmp/LuaJIT
+git clone https://github.com/LuaJIT/LuaJIT.git /tmp/LuaJIT
 pushd /tmp/LuaJIT
 make
+sudo make install
+luajit-2.1.0-beta3 -v
+sudo ln -sf luajit-2.1.0-beta3 /usr/local/bin/luajit
 popd
-cp /tmp/LuaJIT/src/luajit ./bench/include/lua/out
-./bench/include/lua/out/luajit -v
+luajit -v
+# cp /tmp/LuaJIT/src/luajit ./bench/include/lua/out
+# ./bench/include/lua/out/luajit -v
