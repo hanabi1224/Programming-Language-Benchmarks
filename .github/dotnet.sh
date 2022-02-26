c7dcffdb148544c5d90a7e274eba6bdd8e630675
hanabi1224 <harlowmoo@gmail.com>
2022-02-26 23:38:29 +0800

    fix

--- .github/dotnet.sh
+++ .github/dotnet.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo apt-add-repository https://packages.microsoft.com/debian/10/prod
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+sudo apt-add-repository https://packages.microsoft.com/ubuntu/20.04/prod
 sudo apt-get update
 sudo apt-get install -y libmsquic
