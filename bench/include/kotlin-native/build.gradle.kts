de58f0ec355cad32fc64c1df5c7125fc584bf3c4
hanabi1224 <harlowmoo@gmail.com>
2022-04-02 16:11:25 +0800

    kotlin 1.6.20

--- bench/include/kotlin-native/build.gradle.kts
+++ bench/include/kotlin-native/build.gradle.kts
@@ -1,8 +1,9 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.config.KotlinCompilerVersion
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
-  val kotlinVersion = "1.6.10"
+  val kotlinVersion = "1.6.20"
   kotlin("multiplatform").version(kotlinVersion)
   kotlin("plugin.serialization").version(kotlinVersion)
   id("com.github.ben-manes.versions").version("0.42.0")
@@ -27,6 +28,21 @@ kotlin {
         implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
       }
     }
+  }
+}
+
+// https://github.com/JetBrains/kotlin/blob/master/kotlin-native/NEW_MM.md
+// To verify:
+// // main.kt
+// @kotlin.ExperimentalStdlibApi
+// fun main(args: Array<String>) {
+//   println(kotlin.native.isExperimentalMM())
+// }
+//
+kotlin.targets.withType(KotlinNativeTarget::class.java) {
+  binaries.all {
+    binaryOptions["memoryModel"] = "experimental"
+    // freeCompilerArgs += "-Xruntime-logs=gc=info"
   }
 }
 
