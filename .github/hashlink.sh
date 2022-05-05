27c7e146f9d68b1ff1b797585c3f9f4ff082c893
hanabi1224 <harlowmoo@gmail.com>
2022-05-05 22:23:32 +0800

    debug

--- .github/hashlink.sh
+++ .github/hashlink.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+echo $LD_LIBRARY_PATH
+echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH' >>$PROFILE
+source $PROFILE
+echo $LD_LIBRARY_PATH
+
 sudo apt-get install -y libpng-dev libturbojpeg-dev libvorbis-dev libopenal-dev libsdl2-dev libmbedtls-dev libuv1-dev libsqlite3-dev
 git clone https://github.com/HaxeFoundation/hashlink.git /tmp/hashlink
 cd /tmp/hashlink
