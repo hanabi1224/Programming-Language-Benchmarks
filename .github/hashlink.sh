f5dbdbc308de5f9b4803817b833dc1d9b27c8ed2
hanabi1224 <harlowmoo@gmail.com>
2022-05-05 21:58:51 +0800

    fix build

--- .github/hashlink.sh
+++ .github/hashlink.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+sudo apt-get install -y libpng-dev libturbojpeg-dev libvorbis-dev libopenal-dev libsdl2-dev libmbedtls-dev libuv1-dev libsqlite3-dev
 git clone https://github.com/HaxeFoundation/hashlink.git /tmp/hashlink
 cd /tmp/hashlink
 make
