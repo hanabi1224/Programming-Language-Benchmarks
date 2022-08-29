262a2b7bb837dc96c5a4b90fd9300467f67c4c2c
Amir hossein Mahboubi <7353420+sec-researcher@users.noreply.github.com>
2022-08-29 14:51:10 +0430

    Update 1.rs

--- bench/algorithm/helloworld/1.rs
+++ bench/algorithm/helloworld/1.rs
@@ -1,3 +1,6 @@
-fn main() {    
-    println!("Hello world {}!", std::env::args().nth(1).unwrap_or("".to_owned()));
+fn main() {
+    println!(
+        "Hello world {}!",
+        std::env::args().nth(1).unwrap_or_default()
+    );
 }
