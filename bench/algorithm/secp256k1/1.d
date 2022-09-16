25cb6c7164582155a5a3ef263493c9024355623f
Serg Gini <kornburn@gmail.com>
2022-09-16 22:20:09 +0300

    Clear some code
    Mostly approach is from 1.rs

--- bench/algorithm/secp256k1/1.d
+++ bench/algorithm/secp256k1/1.d
@@ -2,51 +2,43 @@
 
 import std;
 
-static ZERO = BigInt(0);
-static ONE = BigInt(1);
-static TWO = BigInt(2);
-static THREE = BigInt(3);
-static EIGHT = BigInt(8);
-static P = BigInt("115792089237316195423570985008687907853269984665640564039457584007908834671663");
-static N = BigInt("115792089237316195423570985008687907852837564279074904382605163141518161494337");
-static GX = BigInt("55066263022277343669578718895168534326250603453777594175500187360389116729240");
-static GY = BigInt("32670510020758816978083085130507043184471273380659243275938904335757337482424");
-static BETA = BigInt("55594575648329892869085402983802832744385952214688224221778511981742606582254");
-static POW_2_128 = BigInt("340282366920938463463374607431768211456");
-static A1 = BigInt("64502973549206556628585045361533709077");
-static B1 = BigInt("-303414439467246543595250775667605759171");
-static A2 = BigInt("367917413016453100223835821029139468248");
-static PRIVATE_KEY = BigInt("20775598474904240222758871485654738649026525153462921990999819694398496339603");
+static immutable ZERO = BigInt(0);
+static immutable ONE = BigInt(1);
+static immutable TWO = BigInt(2);
+static immutable THREE = BigInt(3);
+static immutable EIGHT = BigInt(8);
+static immutable P = BigInt("115792089237316195423570985008687907853269984665640564039457584007908834671663");
+static immutable N = BigInt("115792089237316195423570985008687907852837564279074904382605163141518161494337");
+static immutable BETA = BigInt("55594575648329892869085402983802832744385952214688224221778511981742606582254");
+static immutable POW_2_128 = BigInt("340282366920938463463374607431768211456");
+static immutable A1 = BigInt("64502973549206556628585045361533709077");
+static immutable B1 = BigInt("-303414439467246543595250775667605759171");
+static immutable A2 = BigInt("367917413016453100223835821029139468248");
+static immutable PRIVATE_KEY = BigInt("20775598474904240222758871485654738649026525153462921990999819694398496339603");
+static ZERO_J = JacobianPoint(BigInt(0), BigInt(1), BigInt(0));
 
 void main(string[] args)
 {
-
     auto n = args.length > 1 ? args[1].to!int : 1;
-    Point p;
-    auto point = p.BASE_P();
+    static point = Point(BigInt("55066263022277343669578718895168534326250603453777594175500187360389116729240"),
+                         BigInt("32670510020758816978083085130507043184471273380659243275938904335757337482424"));
     foreach(_; 0..n)
         point = point.multiply(PRIVATE_KEY);
     writefln("%s,%s",replace(toLower(point.x.toHex()),"_",""), replace(toLower(point.y.toHex()),"_",""));
 }
 
-//Point ZERO_P = Point(ZERO, ZERO);
-//Point BASE_P = Point(GX, GY);
-
-//JacobianPoint ZERO_J = JacobianPoint(ZERO, ONE, ZERO);
-//JacobianPoint BASE_J = JacobianPoint(GX, GY, ONE);
-
-BigInt mod(BigInt ma, BigInt mb = P) {
-    auto r = ma % mb;
-    return r < ZERO ? r + mb : r;
+BigInt mod(BigInt a, BigInt b = P) {
+    auto r = a % b;
+    return r < ZERO ? r + b : r;
 }
 
 BigInt invert(BigInt number, BigInt modulo = P) {
-    auto a = mod(number, modulo);
-    auto b = modulo;
-    auto x = ZERO;
-    auto y = ONE;
-    auto u = ONE;
-    auto v = ZERO;
+    BigInt a = mod(number, modulo);
+    BigInt b = modulo;
+    BigInt x = ZERO;
+    BigInt y = ONE;
+    BigInt u = ONE;
+    BigInt v = ZERO;
     while (a != ZERO) {
         auto q = b / a;
         auto r = b % a;
@@ -81,22 +73,13 @@ auto splitScalarEndo (BigInt k) {
     return tuple(k1neg, k1, k2neg, k2);
 }
 
-
 struct JacobianPoint {
     BigInt x,y,z;
 
-    this(BigInt X, BigInt Y, BigInt Z) {
+    this (BigInt X, BigInt Y, BigInt Z) {
         this.x = X;
         this.y = Y;
         this.z = Z;
-    }
-
-    JacobianPoint ZERO_J() {
-        return JacobianPoint(ZERO, ONE, ZERO);
-    }
-
-    JacobianPoint BASE_J() {
-        return JacobianPoint(GX, GY, ONE);
     }
 
     JacobianPoint fromAffine(Point p) {
@@ -116,38 +99,29 @@ struct JacobianPoint {
     }
 
     JacobianPoint double_mul() {
-        auto X1 = this.x;
-        auto Y1 = this.y;
-        auto Z1 = this.z;
-        auto A = mod(X1 * X1);
-        auto B = mod(Y1 * Y1);
-        auto C = mod(B * B);
-        auto D = mod(TWO * (mod(mod((X1+B) ^^ 2)) - A - C));
+        auto A = mod(this.x ^^ 2);
+        auto B = mod(this.y ^^ 2);
+        auto C = mod(B ^^ 2);
+        auto D = mod(TWO * (mod(mod((this.x+B) ^^ 2)) - A - C));
         auto E = mod(THREE * A);
         auto F = mod(E ^^ 2);
         auto X3 = mod(F - TWO * D);
         auto Y3 = mod(E * (D - X3) - EIGHT * C);
-        auto Z3 = mod(TWO * Y1 * Z1);
+        auto Z3 = mod(TWO * this.y * this.z);
         return JacobianPoint(X3, Y3, Z3);
     }
 
     JacobianPoint add(JacobianPoint other) {
-        auto X1 = this.x;
-        auto Y1 = this.y;
-        auto Z1 = this.z;
-        auto X2 = other.x;
-        auto Y2 = other.y;
-        auto Z2 = other.z;
-        if (X2 == ZERO || Y2 == ZERO)
+        if (other.x == ZERO || other.y == ZERO)
             return this;
-        if (X1 == ZERO || Y1 == ZERO)
+        if (this.x == ZERO || this.y == ZERO)
             return other;
-        auto Z1Z1 = mod(Z1 ^^ 2);
-        auto Z2Z2 = mod(Z2 ^^ 2);
-        auto U1 = mod(X1 * Z2Z2);
-        auto U2 = mod(X2 * Z1Z1);
-        auto S1 = mod(Y1 * Z2 * Z2Z2);
-        auto S2 = mod(mod(Y2 * Z1) * Z1Z1);
+        auto Z1Z1 = mod(this.z ^^ 2);
+        auto Z2Z2 = mod(other.z ^^ 2);
+        auto U1 = mod(this.x * Z2Z2);
+        auto U2 = mod(other.x * Z1Z1);
+        auto S1 = mod(this.y * other.z * Z2Z2);
+        auto S2 = mod(mod(other.y * this.x) * Z1Z1);
         auto H = mod(U2 - U1);
         auto r = mod(S2 - S1);
         // H = 0 meaning it's the same point
@@ -155,14 +129,14 @@ struct JacobianPoint {
             if (r == ZERO)
                 return this.double_mul();
             else
-                return JacobianPoint.ZERO_J();
+                return ZERO_J;
         }
         auto HH = mod(H ^^ 2);
         auto HHH = mod(H * HH);
         auto V = mod(U1 * HH);
         auto X3 = mod(r ^^ 2 - HHH - TWO * V);
         auto Y3 = mod(r * (V - X3) - S1 * HHH);
-        auto Z3 = mod(Z1 * Z2 * H);
+        auto Z3 = mod(this.z * other.z * H);
         return JacobianPoint(X3, Y3, Z3);
     }
 
@@ -170,8 +144,8 @@ struct JacobianPoint {
         bool k1neg, k2neg;
         BigInt k1, k2;
         AliasSeq!(k1neg,k1,k2neg,k2)  = splitScalarEndo(n);
-        auto k1p = JacobianPoint.ZERO_J();
-        auto k2p = JacobianPoint.ZERO_J();
+        auto k1p = ZERO_J;
+        auto k2p = ZERO_J;
         auto d = this;
         while (k1 > ZERO || k2 > ZERO) {
             if (k1 & 1)
@@ -199,14 +173,6 @@ struct Point {
         this.y = Y;
     }
 
-    Point ZERO_P() {
-        return Point(ZERO, ZERO);
-    }
-
-    Point BASE_P() {
-        return Point(GX, GY);
-    }
-
     JacobianPoint toPrejective() {
         return JacobianPoint(this.x, this.y, ONE);
     }
@@ -214,6 +180,11 @@ struct Point {
     Point multiply(BigInt scalar) {
         JacobianPoint tmp;
         return tmp.fromAffine(this).multiplyUnsafe(scalar).toAffine();
+    }
+
+    Point opBinary(string op : "*")(BigInt scalar)
+    {
+        return JacobianPoint.fromAffine(this).multiplyUnsafe(scalar).toAffine();
     }
 }
 
