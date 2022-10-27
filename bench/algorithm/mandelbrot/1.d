7e060c65ce4de7f3cd1ccb1c9199af71833972f3
Serg Gini <kornburn@gmail.com>
2022-10-27 12:24:47 +0300

    Fix broadwell issue in Mandelbrot
    I've deleted SIMD realization, because it seems that there is no support in D of AVX512-specific instructions  currently.
    Previously when I've made mandelbrot - I've tried 'double8' and it worked (although in documentation only 'double4' is mentioned). I don't know how exactly LDC is figuring out it internally.

--- bench/algorithm/mandelbrot/1.d
+++ bench/algorithm/mandelbrot/1.d
@@ -6,78 +6,39 @@ import std.digest.md;
 
 static immutable VLEN = 8;
 
-version(LDC){
-    import core.simd;
-    alias Vec = double8;
-    
-    pragma(inline, true):
-    ubyte mbrot8(Vec cr, double civ) {
-        immutable Vec ci = civ;
-        Vec zr = 0.0;
-        Vec zi = 0.0;
-        Vec tr = 0.0;
-        Vec ti = 0.0;
-        Vec absz = 0.0;
-        foreach(l; 0 .. 10) {
-            foreach(k; 0 .. 5) {
-                zi = (zr + zr) * zi + ci;
-                zr = tr - ti + cr;
-                tr = zr * zr;
-                ti = zi * zi;
-            }
-            absz = tr + ti;
-            bool terminate = true;
-            foreach(i; 0 .. 8)
-                if (absz[i] <= 4.0) {
-                    terminate = false;
-                    break;
-                }
-            if (terminate)
-                return 0u;
-        }
-        ubyte accu;
-        foreach(i; 0 .. VLEN)
-            if (absz[i] <= 4.0) {
-                accu |= 0x80 >> i;
-            }
-        return accu;
-    }
-}
-else {
-    alias Vec = double[8];
+alias Vec = double[8];
 
-    pragma(inline, true):
-    ubyte mbrot8(Vec cr, double civ) {
-        immutable Vec ci = civ;
-        Vec zr = 0.0;
-        Vec zi = 0.0;
-        Vec tr = 0.0;
-        Vec ti = 0.0;
-        Vec absz = 0.0;
-        foreach(l; 0 .. 10) {
-            foreach(k; 0 .. 5) {
-                zi[] = (zr[] + zr[]) * zi[] + ci[];
-                zr[] = tr[] - ti[] + cr[];
-                tr[] = zr[] * zr[];
-                ti[] = zi[] * zi[];
-            }
-            absz[] = tr[] + ti[];
-            bool terminate = true;
-            foreach(i; 0 .. 8)
-                if (absz[i] <= 4.0) {
-                    terminate = false;
-                    break;
-                }
-            if (terminate)
-                return 0u;
+pragma(inline, true):
+ubyte mbrot8(Vec cr, double civ) {
+    immutable Vec ci = civ;
+    Vec zr = 0.0;
+    Vec zi = 0.0;
+    Vec tr = 0.0;
+    Vec ti = 0.0;
+    Vec absz = 0.0;
+    foreach(l; 0 .. 10) {
+        foreach(k; 0 .. 5) {
+            zi[] = (zr[] + zr[]) * zi[] + ci[];
+            zr[] = tr[] - ti[] + cr[];
+            tr[] = zr[] * zr[];
+            ti[] = zi[] * zi[];
         }
-        ubyte accu;
-        foreach(i; 0 .. VLEN)
+        absz[] = tr[] + ti[];
+        bool terminate = true;
+        foreach(i; 0 .. 8)
             if (absz[i] <= 4.0) {
-                accu |= 0x80 >> i;
+                terminate = false;
+                break;
             }
-        return accu;
+        if (terminate)
+            return 0u;
     }
+    ubyte accu;
+    foreach(i; 0 .. VLEN)
+        if (absz[i] <= 4.0) {
+            accu |= 0x80 >> i;
+        }
+    return accu;
 }
 
 void main(string[] args) {
