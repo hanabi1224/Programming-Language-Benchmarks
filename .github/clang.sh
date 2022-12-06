34cafd01f72c6f94cb490b89354bf6295347f6ad
hanabi1224 <harlowmoo@gmail.com>
2022-12-06 15:12:03 +0800

    chore: upgrade to ubuntu jammy

--- .github/clang.sh
+++ .github/clang.sh
@@ -2,9 +2,9 @@
 
 # https://apt.llvm.org/
 # sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
-wget https://apt.llvm.org/llvm.sh
-chmod +x llvm.sh
-sudo ./llvm.sh 14
-sudo apt-get install -y clang-14 libapr1-dev libomp-14-dev
-sudo ln -sf /usr/bin/clang-14 /usr/bin/clang
-clang --version
+# wget https://apt.llvm.org/llvm.sh
+# chmod +x llvm.sh
+# sudo ./llvm.sh 14
+sudo apt-get install -y libapr1-dev libomp-14-dev
+# sudo ln -sf /usr/bin/clang-14 /usr/bin/clang
+# clang --version
