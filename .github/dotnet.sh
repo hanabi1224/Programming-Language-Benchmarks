9899a8ad22ed1b58f27c9e591313df6b433b3e8d
hanabi1224 <harlowmoo@gmail.com>
2022-12-08 01:20:26 +0800

    chore: upgrade to ubuntu jammy (#334)

--- .github/dotnet.sh
+++ .github/dotnet.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
-sudo apt-add-repository https://packages.microsoft.com/ubuntu/20.04/prod
+sudo apt-add-repository https://packages.microsoft.com/ubuntu/22.04/prod
 sudo apt-get update
 sudo apt-get install -y libmsquic zlib1g-dev
