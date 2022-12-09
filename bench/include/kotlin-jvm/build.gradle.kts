083ecc3657c96edee5d1368d0149e89cdfc9d64a
hanabi1224 <harlowmoo@gmail.com>
2022-12-09 19:48:00 +0800

    feat: more http-server (#336)

--- bench/include/kotlin-jvm/build.gradle.kts
+++ bench/include/kotlin-jvm/build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation("org.jetbrains.kotlinx:atomicfu:0.18.5")
-    val ktor_version = "2.2.0"
+    val ktor_version = "2.2.1"
     implementation("io.ktor:ktor-server-core:$ktor_version")
     // implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("io.ktor:ktor-server-cio:$ktor_version")
