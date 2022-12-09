4dca840db7e91daa86cf24072fd40bfdb6b55922
hanabi1224 <harlowmoo@gmail.com>
2022-12-09 19:32:02 +0800

    feat: more http-server

--- .github/v.sh
+++ .github/v.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 
 # docker run --rm -v /tmp/hostlib/:/working/ -w /working/ thevlang/vlang:alpine cp /lib/ld-musl-x86_64.so.1 .
 # sudo cp /tmp/hostlib/ld-musl-x86_64.so.1 /lib/
 sudo apt-get install -y libgc-dev
 git clone https://github.com/vlang/v /tmp/vlang
 cd /tmp/vlang
+pushd vlib/vweb
+cat vweb.v | sed "s/\bprintln('\[Vweb\] Running/eprintln('[Vweb] Running/" > temp.v && mv temp.v vweb.v
+popd
 make && ./v -version
 ./v symlink
 v --version
