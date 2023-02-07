4bb46c329420b9513454711222e51d36e8650e08
Christian Kleineidam <christian.kleineidam@gmail.com>
2023-02-07 23:24:24 +0100

    A faster Dart implementation
    The existing Dart implementation gets slowed down a lot because it unnecessarily used async calls. I translated https://github.com/hanabi1224/Programming-Language-Benchmarks/blob/main/bench/algorithm/coro-prime-sieve/1.lua into Dart.

---
+++ bench/algorithm/coro-prime-sieve/3.dart
@@ -0,0 +1,38 @@
+// The Computer Language Benchmarks Game
+// https://salsa.debian.org/benchmarksgame-team/benchmarksgame/
+//
+// contributed by Christian Kleineidam
+// based on the lua version by hanabi1224's lua implementation
+
+Iterable<int> generate() sync*  {
+  int i = 2;
+  while (true) {
+    yield i++;
+  }
+}
+
+Iterable<int> filter(Iterator<int> it, int prime) sync* {
+  while (true){
+    final ok = it.moveNext();
+    if (ok==false){
+      print(it.current);
+      return;
+    }
+    if (it.current % prime != 0){
+      yield it.current;
+    }
+  }
+}
+
+void main(List<String> args) {
+  int n = args.isNotEmpty ? int.parse(args[0]) : 1000;
+  var it = generate().iterator;
+  
+  for (int i = 0; i < n; i++) {
+    it.moveNext();
+    var prime = it.current;
+    print(prime);
+
+    it = filter(it, prime).iterator;
+  }
+}
