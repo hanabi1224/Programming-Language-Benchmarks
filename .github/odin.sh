96657f1710ddd8784006252d6e28f058a0ada472
hanabi1224 <harlowmoo@gmail.com>
2023-02-09 01:59:57 +0800

    chore: upgrade odin

--- .github/odin.sh
+++ .github/odin.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/sh
 
-VERSION=dev-2023-01
+VERSION=dev-2023-02
 FILE_NAME=odin-ubuntu-amd64-$VERSION.zip
-
+sudo apt-get install -y aria2
 mkdir /tmp/odin
 cd /tmp/odin
-wget https://github.com/odin-lang/Odin/releases/download/$VERSION/$FILE_NAME
+aria2c -c -o $FILE_NAME https://github.com/odin-lang/Odin/releases/download/$VERSION/$FILE_NAME
 unzip -o $FILE_NAME
 sudo chmod +x $PWD/odin
 sudo ln -sf $PWD/odin /usr/bin/odin
