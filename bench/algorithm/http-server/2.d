9867097a61751a702aecab663fe08097784339bc
Serg Gini <kornburn@gmail.com>
2023-02-22 01:50:45 +0400

    Extra version
    With explicit JSON sending and parsing

---
+++ bench/algorithm/http-server/2.d
@@ -0,0 +1,35 @@
+import std;
+import arsd.cgi;
+
+static void startServer(RequestServer s) {
+    s.serve!(postFunc, Cgi, defaultMaxContentLength);
+}
+
+void postFunc(Cgi cgi) {
+    cgi.setResponseContentType("text/plain");
+    auto p = cgi.post.keys[0].parseJSON()["value"];
+    cgi.write(p.toString);
+}
+
+int reqSend(Tuple!(string, int) val) {
+    while (true) {
+        auto content = std.net.curl.post(val[0], (JSONValue(["value" : val[1]])).toString);
+        return content.to!int;
+    }
+}
+
+int main(string[] args)
+{
+    int n = args.length > 1 ? args[1].to!int : 10;
+    auto rnd = Random(unpredictableSeed);
+    auto port = uniform(30000, 40000, rnd);
+    string api = "127.0.0.1:"~to!string(port)~"/";
+    RequestServer server = RequestServer("127.0.0.1", cast(ushort) port);
+    server.useFork = false;
+    auto tid = spawn(&startServer, server);
+    auto init = iota(1,n + 1,1).map!(a => tuple(api, a));
+    auto res = taskPool.amap!reqSend(init, n);
+    writeln(res.sum);
+    server.stop();
+    return 0;
+}
