3b12323e93e992d839d66a28e1eb898c08c4ffdb
acefsm <multistream@gmail.com>
2023-02-23 00:46:22 +0300

    single-threaded solution

--- bench/algorithm/binarytrees/1.swift
+++ bench/algorithm/binarytrees/1.swift
@@ -8,32 +8,21 @@ if CommandLine.argc > 1 {
 
 let minDepth = UInt32(4)
 let maxDepth = (n > minDepth + 2) ? n : minDepth + 2
-let depth = maxDepth + 1
-let check = Tree(depth: depth).check
-print("stretch tree of depth \(maxDepth+1)\t check: \(check)")
+let stretchDepth = maxDepth + 1
+let check = Tree(depth: stretchDepth).check
+print("stretch tree of depth \(stretchDepth)\t check: \(check)")
 
-var messages = [(UInt32, String)]()
 let longLivingTree = Tree(depth: maxDepth)
-await withTaskGroup(of: (UInt32, String).self) { group in
-    for d in stride(from: minDepth, to: maxDepth, by: 2) {
-        group.addTask {
-            let iterations = UInt32(1 << (maxDepth - d + minDepth))
-            var chk: UInt32 = 0
-            for _ in 1...iterations {
-                chk += Tree(depth: d).check
-            }
-            return (d, "\(iterations)\t trees of depth \(d)\t check: \(chk)")
-        }
+
+for depth in stride(from: minDepth, to: maxDepth, by: 2) {
+    let iterations = UInt32(1 << (maxDepth - depth + minDepth))
+    var chk: UInt32 = 0
+    for _ in 1...iterations {
+        chk += Tree(depth: depth).check
     }
-    
-    for await msg in group {
-        messages.append(msg)
-    }
+    print("\(iterations)\t trees of depth \(depth)\t check: \(chk)")
 }
 
-for msg in messages.sorted(by: { $0.0 < $1.0 }) {
-    print(msg.1)
-}
 print("long lived tree of depth \(maxDepth)\t check: \(longLivingTree.check)")
 
 indirect enum Tree {
