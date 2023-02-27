70df07c49d09b76deb2e76c96644064c0b3111fc
acefsm <multistream@gmail.com>
2023-02-27 03:00:22 +0300

    fixed algo

--- bench/algorithm/binarytrees/1.swift
+++ bench/algorithm/binarytrees/1.swift
@@ -1,20 +1,15 @@
 import Foundation
 
-var n = UInt32(10)
-
-if CommandLine.argc > 1 {
-    n = UInt32(CommandLine.arguments[1]) ?? UInt32(10)
-}
-
 let minDepth = UInt32(4)
-let maxDepth = (n > minDepth + 2) ? n : minDepth + 2
+let maxDepth = CommandLine.argc > 1 ? max(UInt32(CommandLine.arguments[1]) ?? 0, minDepth + 2) : 10
 let stretchDepth = maxDepth + 1
 let check = Tree(depth: stretchDepth).check
 print("stretch tree of depth \(stretchDepth)\t check: \(check)")
 
 let longLivingTree = Tree(depth: maxDepth)
 
-for depth in stride(from: minDepth, to: maxDepth, by: 2) {
+for halfDepth in (minDepth / 2)..<(maxDepth / 2 + 1) {
+    let depth = halfDepth * 2
     let iterations = UInt32(1 << (maxDepth - depth + minDepth))
     var chk: UInt32 = 0
     for _ in 1...iterations {
