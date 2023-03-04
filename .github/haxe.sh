e2de7d7348181f209cf85c8b2a2e049878cf32e9
hanabi1224 <harlowmoo@gmail.com>
2023-03-04 12:14:45 +0800

    fix bench step

--- .github/haxe.sh
+++ .github/haxe.sh
@@ -3,6 +3,7 @@
 sudo add-apt-repository ppa:haxe/releases -y
 sudo apt-get update
 sudo apt-get install haxe -y
+haxelib --global update haxelib
 mkdir ~/haxelib && haxelib setup ~/haxelib
 haxelib install hxcpp
 haxelib git hashlink https://github.com/HaxeFoundation/hashlink.git master other/haxelib/
--- bench/tool/Program.cs
+++ bench/tool/Program.cs
@@ -514,6 +514,7 @@ namespace BenchTool
             {
                 string exeRoot = GetIncludedRuntimeRoot(langEnvConfig: langEnvConfig, buildOutputRoot: buildOutputRoot, buildOutput: buildOutput);
                 exeName = Path.Combine(exeRoot, exeName);
+                await ProcessUtils.RunCommandAsync($"chmod +x \"{exeName}\"", asyncRead: false, workingDir: buildOutput).ConfigureAwait(false);
             }
 
             string runtimeVersionParameter = langEnvConfig.RuntimeVersionParameter.FallBackTo(langConfig.RuntimeVersionParameter);
@@ -625,6 +626,7 @@ namespace BenchTool
             {
                 string exeRoot = GetIncludedRuntimeRoot(langEnvConfig: langEnvConfig, buildOutputRoot: buildOutputRoot, buildOutput: buildOutput);
                 exeName = Path.Combine(exeRoot, exeName);
+                await ProcessUtils.RunCommandAsync($"chmod +x \"{exeName}\"", asyncRead: false, workingDir: buildOutput).ConfigureAwait(false);
             }
 
             YamlBenchmarkProblemConfig problemTestConfig = benchConfig.Problems.FirstOrDefault(i => i.Name == problem.Name);
