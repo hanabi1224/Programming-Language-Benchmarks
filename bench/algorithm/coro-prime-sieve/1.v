3a0a0b753ae0311e8d7da0ddf6c023feb86af261
encyclopaedia <prashanth-hegde@users.noreply.github.com>
2023-07-09 20:26:52 -0500

    Update 1.v for efficiency
    Updating the sieve without using `spawn` or `channels` as V spawn now creates new OS threads instead of green threads.
    And coroutines is still a WIP. 
    
    Updating the code for simple sieve calculation

--- bench/algorithm/coro-prime-sieve/1.v
+++ bench/algorithm/coro-prime-sieve/1.v
@@ -1,37 +1,25 @@
 module main
 
 import os
-import strconv
 
 fn main() {
-	mut n := 100
-	if os.args.len > 1 {
-		n = strconv.atoi(os.args[1]) or { n }
-	}
+  n := if os.args.len > 1 {
+    os.args[1].int()
+  } else {
+    100
+  }
 
-	mut ch := chan int{cap: 1}
-	spawn generate(ch)
-	for _ in 0 .. n {
-		prime := <-ch
-		println(prime)
-		ch_next := chan int{cap: 1}
-		spawn filter(ch, ch_next, prime)
-		ch = ch_next
-	}
-}
-
-fn generate(ch chan int) {
-	mut i := 2
-	for {
-		ch <- i++
-	}
-}
-
-fn filter(chin chan int, chout chan int, prime int) {
-	for {
-		i := <-chin
-		if i % prime != 0 {
-			chout <- i
-		}
-	}
+  mut sieve := []bool{len: n+1, init: true}
+  for i := 2; i*i <= n; i++ {
+    if sieve[i] {
+      for j := i*i; j <= n; j += i {
+        sieve[j] = false
+      }
+    }
+  }
+  for i := 2; i <= n; i++ {
+    if sieve[i] {
+      println(i)
+    }
+  }
 }
