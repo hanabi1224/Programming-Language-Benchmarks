59f94b275005d6c0213beb464aeb03cc98d607d0
Brian Gilreath <briangilreath@icloud.com>
2023-12-20 10:39:04 -0800

    Fix Lua Setup Script Re Bug #422

--- .github/lua.sh
+++ .github/lua.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/bash
-
+set -ev
 # https://www.lua.org/ftp/#
 pushd /tmp
-curl -R -O http://www.lua.org/ftp/lua-5.4.4.tar.gz
-tar zxf lua-5.4.4.tar.gz
-pushd lua-5.4.4
+curl -R -O http://www.lua.org/ftp/lua-5.4.6.tar.gz
+tar zxf lua-5.4.6.tar.gz
+pushd lua-5.4.6
 make all test
 # sudo ln -sf $PWD/src/luac /usr/bin/luac
 # sudo ln -sf $PWD/src/lua /usr/bin/lua
 sudo make install
 popd
 popd
-# cp /tmp/lua-5.4.4/src/lua ./bench/include/lua/out
+# cp /tmp/lua-5.4.6/src/lua ./bench/include/lua/out
 luac -v
 lua -v
 # ./bench/include/lua/out/lua -v
 
 git clone https://github.com/LuaJIT/LuaJIT.git /tmp/LuaJIT
 pushd /tmp/LuaJIT
+git checkout tags/v2.1.0-beta3
 make
 sudo make install
 luajit-2.1.0-beta3 -v
